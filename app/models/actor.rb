2e8bebb2237021104f354f2e7fca4620d3160eb5
Sean Altemose <sean@seanaltemose.com>
2018-06-26 16:42:21 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,27 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    return self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    characters = []
+    show_names = []
+
+    self.characters.each do |char_instance|
+      characters << char_instance.name
+    end
+
+    self.shows.each do |show_instance|
+      show_names << show_instance.name
+    end
+
+    characters = characters.join()
+    show_names = show_names.join()
+
+    return characters + " - " + show_names
+  end
+
 end
